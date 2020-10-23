2d22ee889904d9edf475fd813e4ee03c1c796c2e
studentprogrammer17 <72373586+studentprogrammer17@users.noreply.github.com>
2020-10-23 21:26:27 +0300

    Update cash.c

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -14,9 +14,9 @@ int main()
     while (cents_owed <= 0);
 
     int quarters = cents_owed / 25;
-    int dimes = (cents_owed % 25) / 10;
-    int nickels = ((cents_owed % 25) % 10) / 5;
-    int pennies = ((cents_owed % 25) % 10) % 5;
+    int dimes = cents_owed / 10;
+    int nickels = cents_owed / 5;
+    int pennies = cents_owed / 1;
 
     printf("%d\n", quarters + dimes + nickels + pennies);
 }
